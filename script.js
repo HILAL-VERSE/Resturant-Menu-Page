c00ce1f1e07c061c3fdadf7d846e94047fe0e396
HILAL-VERSE <adkhilal@gmail.com>
2025-11-07 19:35:08 +0530

    Finished

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const productTemplate = document.querySelector('#product-template');
 const insertHere = document.querySelector('#insert-here');
 
 
-for (let i=0; i < 5; i++) {
+for (let i=0; i < 6; i++) {
     const productBox = productTemplate.content.cloneNode(true);
     insertHere.appendChild(productBox);
 }
